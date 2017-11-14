b5fe8e2a620012d2f5ca11d04cdffe6dc31c70bc
randliu <buckler.liu@gmail.com>
2017-11-14 18:27:11 +0800

    helloworld

--- helloworld.py
+++ helloworld.py
@@ -4,10 +4,16 @@ import werobot
 
 robot = werobot.WeRoBot(token='football1898tokenhere')
 
-@robot.handler
-def hello(message):
-	 print message
-	 return 'Hello World!'
+# @robot.text 修饰的 Handler 只处理文本消息
+@robot.text
+def echo(message):
+    return message.content
+
+# @robot.image 修饰的 Handler 只处理图片消息
+@robot.image
+def img(message):
+    return message.img
+
 
 # 让服务器监听在 0.0.0.0:80
 robot.config['HOST'] = '0.0.0.0'
